639359424b2457a9dcca18cb090782fc15160266
Sagar Yadav <sagaryadav6352@gmail.com>
2025-12-17 11:18:59 +0530

    feat: Add redirects and update routing to display Coming Soon page and handle 404 errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import ComingSoon from "./pages/ComingSoon";
+import PageNotFound from "./pages/PageNotFound";
 
 const App: React.FC = () => (
   <Routes>
-    <Route path="*" element={<ComingSoon />} />
+    <Route path="/" element={<ComingSoon />} />
+    <Route path="*" element={<PageNotFound />} />
   </Routes>
 );
 
